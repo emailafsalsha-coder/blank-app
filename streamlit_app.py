d68b5696dac93e814054ef165f291286e106d878
emailafsalsha-coder <emailafsalsha@gmail.com>
2025-10-22 13:01:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,25 +30,14 @@ st.markdown("""
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 """, unsafe_allow_html=True)
 
-# Google Analytics - Working method for Streamlit
+# Microsoft Clarity
 st.markdown("""
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-C7NC1BWTEG"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-C7NC1BWTEG', {
-    'page_title': document.title,
-    'page_location': window.location.href
-  });
-</script>
-
 <script type="text/javascript">
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "YOUR_CLARITY_ID");
+    })(window, document, "clarity", "script", "tu1sfxyo0x");
 </script>
 """, unsafe_allow_html=True)
 
